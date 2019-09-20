5eb12ba6a2417fb80c67f573bb8a148465facc9d
unknown <jinyoung777@gmail.com>
2019-09-21 10:33:49 +1200

    added PostTagController to show blogpost list related to tags

---
+++ app/Http/Controllers/PostTagController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Tag;
+use Illuminate\Http\Request;
+
+class PostTagController extends Controller
+{
+    public function index($tagId)
+    {
+        $tag = Tag::findOrFail($tagId);
+        return view('posts.index', 
+            [
+                'posts' => $tag->blogPosts,
+                'mostCommentedPosts' => [],
+                'mostActive' => [],
+                'mostActiveLastMonth' => [],
+            ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,6 @@ Route::get('/contact', 'HomeController@contact')->name('contact');
 Route::get('/secret', 'HomeController@secret')->name('secret')
     ->middleware('can:home.secret');
 Route::resource('posts', 'PostController');
+Route::get('/posts/tag/{tag}', 'PostTagController@index')->name('posts.tags.index');
 
 Auth::routes();
